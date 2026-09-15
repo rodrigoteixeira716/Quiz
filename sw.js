1c0f1898fb5472c649a567314fe183be7588f6e1
rodrigoteixeira716 <rodrigoteixeira716@gmail.com>
2026-09-15 14:13:02 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='quiz-pwa-v7-600';
+const CACHE_NAME='quiz-pwa-v8-perfis-pin';
 const APP_SHELL=['./index.html','./manifest.json','./icon.svg'];
 
 self.addEventListener('install',event=>{
